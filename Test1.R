499e0221876fec61edc389c34518d6dc249b64e0
ananyboss <anany.7777777@gmail.com>
2020-03-30 15:05:26 +0530

    Added modifications to run the request for each date

--- Test1.R
+++ Test1.R
@@ -5,52 +5,75 @@ library("jsonlite")
 
 #Function to store .rds files for each month
 yearwise_repositories <- function(year1){
-  req <- GET("https://api.github.com/search/repositories?q=language:R&per_page=100&sort=stars&order=desc&page=1")
-  stop_for_status(req)
-  con <- (content(req, "parsed"))
-
-  if (con[['total_count']] > 1000) {
-    warning(con[['total_count']] , " repos. Only first 1000 returned", call. = FALSE)
-  } else {
-    message(con[['total_count']], " repos")
-  }
 
 
-  parts<-strsplit(req$header[['link']],">;")
-  last<-strsplit(parts[[1]][2],"=")
-  totalpages<-strtoi(last[[1]][length(last[[1]])])
+  start <- as.Date(paste(as.character(year1),"-01-01",sep=""),format="%Y-%m-%d")
+  end   <- as.Date(paste(as.character(year1),"-12-31",sep=""),format="%Y-%m-%d")
 
+  theDate <- start
 
-
-  name<-c()
-  repo_id<-c()
-  created_at<-c()
-  month_created<-c()
-
-  for (i in 1:totalpages){
-    req <- GET(paste("https://api.github.com/search/repositories?q=language:R&per_page=100&sort=stars&order=desc&page=",as.character(i),sep=""))
+  while (theDate <= end)
+  {
+    req <- GET(paste("https://api.github.com/search/repositories?q=language:R+created:",theDate,"&per_page=100&sort=stars&order=desc&page=1",sep=""))
     stop_for_status(req)
     con <- (content(req, "parsed"))
-    for(j in 1:100){
-      year_month<-format(as.Date(con[['items']][[j]][['created_at']]),'%Y,%B')
-      year=strsplit(year_month,",")[1][[1]][1]
-      month=strsplit(year_month,",")[1][[1]][2]
-      if(year==year1){
-        name<-c(name,con[['items']][[j]][['name']])
-        repo_id<-c(repo_id,con[['items']][[j]][['id']])
-        created_at<-c(created_at,con[['items']][[j]][['created_at']])
-        month_created<-c(month_created,month)
-      }
-
+    if (con[['total_count']] > 1000) {
+      warning(con[['total_count']] , " repos. Only first 1000 returned for date",theDate, call. = FALSE)
+    } else {
+      message(con[['total_count']], " repos for date ",theDate)
     }
-    print(i)
+    x<-con[['total_count']]
+
+    if(x>100){
+      parts<-strsplit(req$header[['link']],">;")
+      last<-strsplit(parts[[1]][2],"=")
+      totalpages<-strtoi(last[[1]][length(last[[1]])])
+    }
+    else{
+      totalpages<-1
+    }
+
+
+    name<-c()
+    repo_id<-c()
+    created_at<-c()
+    month_created<-c()
+
+    for (i in 1:totalpages){
+      req <- GET(paste("https://api.github.com/search/repositories?q=language:R+created:",theDate,"&per_page=100&sort=stars&order=desc&page=",as.character(i),sep=""))
+      stop_for_status(req)
+      con <- (content(req, "parsed"))
+      if (x>100){
+        for(j in 1:100){
+            name<-c(name,con[['items']][[j]][['name']])
+            repo_id<-c(repo_id,con[['items']][[j]][['id']])
+            created_at<-c(created_at,con[['items']][[j]][['created_at']])
+            month_created<-c(month_created,month)
+        }
+        x<-x-100
+      }
+      else{
+        for(j in 1:x){
+            name<-c(name,con[['items']][[j]][['name']])
+            repo_id<-c(repo_id,con[['items']][[j]][['id']])
+            created_at<-c(created_at,con[['items']][[j]][['created_at']])
+            month_created<-c(month_created,as.character(theDate,format="%b"))
+        }
+      }
+      print(i)
+    }
+
+    # saveRDS(df,paste("repos_of_",year1,".rds"))
+
+    theDate <- theDate + 1
   }
   df<-data.frame(name,repo_id,created_at,month_created)
   month_wise<-split(df,interaction(month_created))
   for(element in names(month_wise)){
     saveRDS(month_wise[[element]],paste("repos_of_",as.character(element),"_",as.character(year1),".rds",sep=""))
   }
-  # saveRDS(df,paste("repos_of_",year1,".rds"))
+
+
 }
 
 #Function to combine all .rds files and output json and csv files
@@ -76,3 +99,5 @@ combine_data <- function(year1){
   json<-toJSON(df)
   write_json(json,paste("repos_of_",as.character(year1),".json",sep=""))
 }
+yearwise_repositories(2014)
+combine_data(2014)
